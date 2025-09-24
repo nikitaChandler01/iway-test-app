d80b5f4e674e426585365ee15509e3172f209489
nm.berdnik <nm.berdnik@tensor.ru>
2025-09-24 21:23:24 +0700

    feat: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react(), tsconfigPaths(), svgr()],
+    server: { port: 5174 },
+});
