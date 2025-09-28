932773b60ff5da7d78ed32abbd2137c10f2f3952
nm.berdnik <nm.berdnik@tensor.ru>
2025-09-28 17:19:33 +0700

    feat: URL  is defined in .env file

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,23 @@
 import tsconfigPaths from "vite-tsconfig-paths";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths(), svgr()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://transstage1.iwayex.com/transnextgen",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+export default defineConfig(({ mode }) => {
+  // Загружаем env-файлы (например, .env.development, .env.production)
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react(), tsconfigPaths(), svgr()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_APP_API_URL, // берем из .env
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
       },
     },
-  },
+  };
 });
 
