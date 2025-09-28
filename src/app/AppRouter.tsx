7923a142898d22894518317a367d4baae15beca1
nm.berdnik <nm.berdnik@tensor.ru>
2025-09-28 17:40:30 +0700

    fix: dispatch events

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -2,19 +2,19 @@ import { userModel } from "@entities/user";
 import { SignIn } from "@pages/SignIn";
 import { apiService } from "@shared/api/api";
 import { useLayoutEffect } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Route, Routes } from "react-router";
 import AuthorizedRouter from "./AuthorizedRouter";
 
 const AppRouter = () => {
   const isAuthorized = useSelector(userModel.selectIsAuthorizated);
   const setIsAuthorized = userModel.setIsAuthorized;
-
+  const dispatch = useDispatch();
   useLayoutEffect(() => {
     const access_token = localStorage.getItem("access_token");
     if (access_token) {
       apiService.setAuthorizationHeader(access_token);
-      setIsAuthorized(true);
+      dispatch(setIsAuthorized(true));
     }
   }, []);
 
@@ -28,4 +28,3 @@ const AppRouter = () => {
 };
 
 export default AppRouter;
-
