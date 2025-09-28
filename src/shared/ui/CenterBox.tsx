69c247aec939436cdbf98f272343d15e167fadc6
nm.berdnik <nm.berdnik@tensor.ru>
2025-09-28 14:50:34 +0700

    feat: centering com,p inited

---
+++ src/shared/ui/CenterBox.tsx
@@ -0,0 +1,8 @@
+import "./CenterBox.scss";
+
+const CenterBox = ({ children }: { children: React.ReactNode }) => {
+  return <div className="center-box">{children}</div>;
+};
+
+export default CenterBox;
+
