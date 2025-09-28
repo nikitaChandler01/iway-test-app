2a5f3c52230f691d11b5c6c26396e4c8466ef0e9
nm.berdnik <nm.berdnik@tensor.ru>
2025-09-28 18:30:10 +0700

    feat: layout with header inited

---
+++ src/app/layout/Layout.tsx
@@ -0,0 +1,28 @@
+import { iwayLogo } from "@assets/icons";
+import { Button, Flex } from "antd";
+import React from "react";
+import "./Layout.scss";
+import { LogoutOutlined } from "@ant-design/icons";
+
+interface ILayout {
+  children: React.ReactNode;
+}
+
+const Layout = ({ children }: ILayout) => {
+  return (
+    <Flex vertical className="layout">
+      <Flex className="layout__header" justify="space-between" align="center">
+        <img src={iwayLogo} className="layout__logo" />
+        <Button
+          className="layout__logout-button"
+          shape="circle"
+          icon={<LogoutOutlined />}
+        />
+      </Flex>
+      <Flex className="layout__content">{children}</Flex>
+    </Flex>
+  );
+};
+
+export default Layout;
+
